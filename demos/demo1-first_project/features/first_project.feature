700246d5cd43bbb6f0515d412559a4a226005935
Jean-Nadir <jeannadir@hotmail.com>
2025-08-18 14:30:08 +0200

    Démo : Mon premier Projet BDD

---
+++ demos/demo1-first_project/features/first_project.feature
@@ -0,0 +1,12 @@
+# Created by jeann at 18/08/2025
+Feature: Mon Premier projet BDD
+
+  L'objectif de ce fichier est de découvrir la syntaxe Gherkin en Python avec Behave.
+
+  Scenario: Test de mon premier scénario
+
+    Given J'ouvre mon terminal
+    When Je me déplace dans le répertoire C:\Users\jeann\PycharmProjects\PythonProject\cours-bdd
+    And J'affiche le contenu avec 'dir'
+    And Je regarde le contenu du répertoire
+    Then Je ferme le répertoire
---
+++ demos/demo1-first_project/features/steps/first_project.py
@@ -0,0 +1,54 @@
+import time
+import logging
+from behave import *
+
+# Création d'un logger pour enregistrer tous les évènements du programme
+logger = logging.getLogger("First_project")
+logger.setLevel(logging.INFO)
+
+# Création d'un formatteur commun
+# Liste des paramètres du Formatter ici :
+# https://docs.python.org/3/library/logging.html#logrecord-attributes
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Création du handler (gestionnaire de sortie)
+file_handler = logging.FileHandler('first_project.log', mode='a', encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)  # Niveau de log minimum à enregistrer dans le fichier
+file_handler.setFormatter(formatter)
+
+# Association du handler au logger
+logger.addHandler(file_handler)
+
+@given("J'ouvre mon terminal")
+def open_terminal(context):
+    print("P: J'ouvre mon terminal.")
+    logger.info("J'ouvre mon terminal.")
+    time.sleep(2)
+
+
+@when(u"Je me déplace dans le répertoire C:\\Users\jeann\PycharmProjects\PythonProject\cours-bdd")
+def deplacer_Dans_Repertoire(context):
+    print('P: Je me déplace dans le répertoire C:\\Users\jeann\PycharmProjects\PythonProject\cours-bdd')
+    logger.info("Je me déplace dans le répertoire C:\\Users\jeann\PycharmProjects\PythonProject\cours-bdd")
+    time.sleep(2)
+
+
+@when("J'affiche le contenu avec 'dir'")
+def affiche_contenu(context):
+    print("P: J'affiche le contenu avec 'dir'")
+    logger.info("J'affiche le contenu avec 'dir'")
+    time.sleep(2)
+
+
+@when("Je regarde le contenu du répertoire")
+def regarde_contenu(context):
+    print("P: Je regarde le contenu du répertoire")
+    logger.info("Je regarde le contenu du répertoire")
+    time.sleep(2)
+
+
+@then("Je ferme le répertoire")
+def ferme_repertoire(context):
+    print("P: Je ferme le répertoire")
+    logger.info("Je ferme le répertoire")
+    time.sleep(2)
